60567ba393863614efba4039fcf427be06a1b917
hobetuz <hobetuz@Gorbea10.ipartekaula.com>
2023-06-27 08:24:00 +0200

    Cambios Formulario

--- Bases/src/EjerciciosTres/Formulario.java
+++ Bases/src/EjerciciosTres/Formulario.java
@@ -4,9 +4,13 @@ import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JList;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class Formulario {
@@ -14,6 +18,8 @@ public class Formulario {
 	private JFrame frmRegistro;
 	private JTextField textField;
 	private JTextField textField_1;
+	private JTextField textField_2;
+	private JMenuBar BarraBtn;
 
 	/**
 	 * Launch the application.
@@ -35,6 +41,33 @@ public class Formulario {
 	 * Create the application.
 	 */
 	public Formulario() {
+
+		BarraBtn = new JMenuBar();
+
+		JMenu archivo = new JMenu("Archivo");
+		JMenu editar = new JMenu("Editar");
+
+		BarraBtn.add(archivo);
+		BarraBtn.add(editar);
+
+		JMenuItem abrir = new JMenuItem("Abrir");
+		JMenuItem guardar = new JMenuItem("Guardar");
+		JMenuItem cargar = new JMenuItem("Cargar");
+		JMenuItem salir = new JMenuItem("Salir");
+
+		JMenuItem modificar = new JMenuItem("Modificar");
+		JMenuItem copiar = new JMenuItem("Copiar");
+		JMenuItem pegar = new JMenuItem("Pegar");
+
+		archivo.add(abrir);
+		archivo.add(guardar);
+		archivo.add(cargar);
+		archivo.add(salir);
+
+		editar.add(modificar);
+		editar.add(copiar);
+		editar.add(pegar);
+
 		initialize();
 	}
 
@@ -42,36 +75,61 @@ public class Formulario {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+
 		frmRegistro = new JFrame();
+		frmRegistro.getContentPane().setFont(new Font("Tahoma", Font.PLAIN, 14));
 		frmRegistro.getContentPane().setBackground(new Color(192, 192, 192));
 		frmRegistro.setTitle("Registro");
 		frmRegistro.setBounds(100, 100, 462, 491);
 		frmRegistro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmRegistro.getContentPane().setLayout(null);
 
+		frmRegistro.setJMenuBar(BarraBtn);
+
 		JLabel lblNombre = new JLabel("Nombre");
-		lblNombre.setFont(new Font("Source Sans Pro ExtraLight", Font.PLAIN, 17));
-		lblNombre.setBounds(26, 11, 65, 23);
+		lblNombre.setBounds(62, 281, 49, 17);
+		lblNombre.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		frmRegistro.getContentPane().add(lblNombre);
 
 		textField = new JTextField();
-		textField.setBounds(114, 14, 115, 20);
+		textField.setBounds(147, 280, 230, 20);
 		frmRegistro.getContentPane().add(textField);
 		textField.setColumns(10);
 
-		JLabel lblPais = new JLabel("Pais");
-		lblPais.setFont(new Font("Source Sans Pro ExtraLight", Font.PLAIN, 17));
-		lblPais.setBounds(26, 51, 65, 23);
-		frmRegistro.getContentPane().add(lblPais);
-
-		@SuppressWarnings("rawtypes")
-		JList list = new JList();
-		list.setBounds(228, 108, -55, -34);
-		frmRegistro.getContentPane().add(list);
+		JLabel lblEdad = new JLabel("Edad");
+		lblEdad.setBounds(71, 306, 31, 17);
+		lblEdad.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		frmRegistro.getContentPane().add(lblEdad);
 
 		textField_1 = new JTextField();
+		textField_1.setBounds(147, 305, 230, 20);
 		textField_1.setColumns(10);
-		textField_1.setBounds(114, 54, 115, 20);
 		frmRegistro.getContentPane().add(textField_1);
+
+		JLabel lblEmpleo = new JLabel("Empleo");
+		lblEmpleo.setBounds(64, 331, 45, 17);
+		lblEmpleo.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		frmRegistro.getContentPane().add(lblEmpleo);
+
+		textField_2 = new JTextField();
+		textField_2.setBounds(147, 330, 230, 20);
+		textField_2.setColumns(10);
+		frmRegistro.getContentPane().add(textField_2);
+
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(62, 112, 315, 127);
+		frmRegistro.getContentPane().add(textArea);
+
+		JButton btnEnviar = new JButton("Enviar");
+		btnEnviar.setBounds(52, 396, 89, 23);
+		frmRegistro.getContentPane().add(btnEnviar);
+
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.setBounds(172, 396, 89, 23);
+		frmRegistro.getContentPane().add(btnCancelar);
+
+		JButton btnSalir = new JButton("Salir");
+		btnSalir.setBounds(299, 396, 89, 23);
+		frmRegistro.getContentPane().add(btnSalir);
 	}
 }
